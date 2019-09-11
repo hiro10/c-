b17869de4ea8270fb412f1a7c88a7ede7bf4ced3
HIRO025 <n.t.hiro1@gmail.com>
2019-09-11 15:39:26 +0900

    コメント修正２

--- ui.h
+++ ui.h
@@ -7,17 +7,22 @@
 #ifndef _UI_H_
 #define _UI_H_
 
+//*****************************************************************************
+// インクルード
+//*****************************************************************************
 #include "main.h"
+
+
 //*****************************************************************************
 // マクロ定義
 //*****************************************************************************
 
 #define	TEXTURE_UI_LOGO1	("data/TEXTURE/敵機接近.png")				// 読み込むテクスチャファイル名
-#define	UILOGO1_POS_X	(470)											// ロゴの表示位置
-#define	UILOGO1_POS_Y	(5)												// ロゴの表示位置
-#define	UILOGO1_POS_Z	(0)												// ロゴの表示位置
-#define	UILOGO1_SIZE_X	(400)											// ロゴの幅
-#define	UILOGO1_SIZE_Y	(100)											// ロゴの高さ
+#define	UILOGO1_POS_X		(470)										// ロゴの表示位置
+#define	UILOGO1_POS_Y		(5)											// ロゴの表示位置
+#define	UILOGO1_POS_Z		(0)											// ロゴの表示位置
+#define	UILOGO1_SIZE_X		(400)										// ロゴの幅
+#define	UILOGO1_SIZE_Y		(100)										// ロゴの高さ
 
 #define	TEXTURE_UIACE_LOGO	("data/TEXTURE/エース出現.png")				// 読み込むテクスチャファイル名
 #define	UIACELOGO1_POS_X	(470)										// ロゴの表示位置
