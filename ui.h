200876f5b829d84180d4a88e160584fb20d3c4cb
HIRO025 <n.t.hiro1@gmail.com>
2019-09-11 15:37:52 +0900

    UIヘッダのコメント内容、その位置の修正（コピペのや内容を独自に修正）

--- ui.h
+++ ui.h
@@ -12,33 +12,33 @@
 // マクロ定義
 //*****************************************************************************
 
-#define	TEXTURE_UI_LOGO1	("data/TEXTURE/敵機接近.png")	// 読み込むテクスチャファイル名
-#define	UILOGO1_POS_X	(470)										// ポーズロゴの表示位置
-#define	UILOGO1_POS_Y	(5)										// ポーズロゴの表示位置
-#define	UILOGO1_POS_Z	(0)											// ポーズロゴの表示位置
-#define	UILOGO1_SIZE_X	(400)										// ポーズロゴの幅
-#define	UILOGO1_SIZE_Y	(100)										// ポーズロゴの高さ
+#define	TEXTURE_UI_LOGO1	("data/TEXTURE/敵機接近.png")				// 読み込むテクスチャファイル名
+#define	UILOGO1_POS_X	(470)											// ロゴの表示位置
+#define	UILOGO1_POS_Y	(5)												// ロゴの表示位置
+#define	UILOGO1_POS_Z	(0)												// ロゴの表示位置
+#define	UILOGO1_SIZE_X	(400)											// ロゴの幅
+#define	UILOGO1_SIZE_Y	(100)											// ロゴの高さ
 
-#define	TEXTURE_UIACE_LOGO	("data/TEXTURE/エース出現.png")	// 読み込むテクスチャファイル名
-#define	UIACELOGO1_POS_X	(470)										// ポーズロゴの表示位置
-#define	UIACELOGO1_POS_Y	(5)										// ポーズロゴの表示位置
-#define	UIACELOGO1_POS_Z	(0)											// ポーズロゴの表示位置
-#define	UIACELOGO1_SIZE_X	(500)										// ポーズロゴの幅
-#define	UIACELOGO1_SIZE_Y	(150)										// ポーズロゴの高さ
+#define	TEXTURE_UIACE_LOGO	("data/TEXTURE/エース出現.png")				// 読み込むテクスチャファイル名
+#define	UIACELOGO1_POS_X	(470)										// ロゴの表示位置
+#define	UIACELOGO1_POS_Y	(5)											// ロゴの表示位置
+#define	UIACELOGO1_POS_Z	(0)											// ロゴの表示位置
+#define	UIACELOGO1_SIZE_X	(500)										// ロゴの幅
+#define	UIACELOGO1_SIZE_Y	(150)										// ロゴの高さ
 
-#define	TEXTURE_UIITEM_LOGO	("data/TEXTURE/ターゲット.png")	// 読み込むテクスチャファイル名
-#define	UIITEMLOGO1_POS_X	(240)										// ポーズロゴの表示位置
-#define	UIITEMLOGO1_POS_Y	(150)										// ポーズロゴの表示位置
-#define	UIITEMLOGO1_POS_Z	(0)											// ポーズロゴの表示位置
-#define	UIITEMLOGO1_SIZE_X	(900)										// ポーズロゴの幅
-#define	UIITEMLOGO1_SIZE_Y	(300)										// ポーズロゴの高さ
+#define	TEXTURE_UIITEM_LOGO	("data/TEXTURE/ターゲット.png")				// 読み込むテクスチャファイル名
+#define	UIITEMLOGO1_POS_X	(240)										// ロゴの表示位置
+#define	UIITEMLOGO1_POS_Y	(150)										// ロゴの表示位置
+#define	UIITEMLOGO1_POS_Z	(0)											// ロゴの表示位置
+#define	UIITEMLOGO1_SIZE_X	(900)										// ロゴの幅
+#define	UIITEMLOGO1_SIZE_Y	(300)										// ロゴの高さ
 
-#define	TEXTURE_UI2_LOGO	("data/TEXTURE/sign-1513493_960_720 (2).png")	// 読み込むテクスチャファイル名
-#define	UILOGO2_POS_X	(0)										// ポーズロゴの表示位置
-#define	UILOGO2_POS_Y	(0)										// ポーズロゴの表示位置
-#define UILOGO2_POS_Z	(0)											// ポーズロゴの表示位置
-#define	UILOGO2_SIZE_X	(SCREEN_WIDTH)										// ポーズロゴの幅
-#define	UILOGO2_SIZE_Y	(SCREEN_HEIGHT)										// ポーズロゴの高さ
+#define	TEXTURE_UI2_LOGO	("data/TEXTURE/ボス演出用警告画面用.png")	// 読み込むテクスチャファイル名
+#define	UILOGO2_POS_X	(0)												// ロゴの表示位置
+#define	UILOGO2_POS_Y	(0)												// ロゴの表示位置
+#define UILOGO2_POS_Z	(0)												// ロゴの表示位置
+#define	UILOGO2_SIZE_X	(SCREEN_WIDTH)									// ロゴの幅
+#define	UILOGO2_SIZE_Y	(SCREEN_HEIGHT)									// ロゴの高さ
 
 
 #define MODE_GAME_COUNT		(30)
